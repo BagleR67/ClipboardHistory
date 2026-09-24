17c7d875123dbd97a0e4f5d0e2656c4709f2f2f7
Kirill <hello2020kirill@gmail.com>
2026-09-24 14:29:12 +0600

    Добавьте файлы проекта.

---
+++ ClipboardHistory/Models/clipModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ClipboardHistory.Models
+{
+    public class clipModel
+    {
+        public string Text { get; set;}
+
+        public DateTime CopiedAt { get; set; } = DateTime.Now;       
+    }
+}
